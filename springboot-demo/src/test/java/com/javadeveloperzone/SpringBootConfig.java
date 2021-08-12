66d682bb507b8a8307427d78141d842824579703
preetas <88144614+preetas@users.noreply.github.com>
2021-08-12 11:13:06 +0530

    Update SpringBootConfig.java

--- springboot-demo/src/test/java/com/javadeveloperzone/SpringBootConfig.java
+++ springboot-demo/src/test/java/com/javadeveloperzone/SpringBootConfig.java
@@ -1,4 +1,4 @@
-package com.javadeveloperzone;
+package test.java.com.javadeveloperzone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
